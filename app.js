31e29c86ab4b067ac5d10a0078e522bcb43f8ca1
Thanco <th@nco.host>
2024-03-14 17:55:43 -0400

    Revert

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 var http = require("http");
-const port = process.env.PORT || 10000;
-const hostname = process.env.HOSTNAME || "0.0.0.0";
+const port = process.env.PORT || 3001;
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("Hello World!");
   })
-  .listen(port, hostname);
-
-console.log(`Server running at http://${hostname}:${port}/`);
+  .listen(port);
