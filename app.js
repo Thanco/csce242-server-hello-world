b26062b1ff41c0ee26399f032e8baba08440297a
Thanco <th@nco.host>
2024-03-14 09:31:45 -0400

    Test Update

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var http = require("http");
-const port = process.env.PORT || 3001;
-// const hostname = process.env.HOSTNAME || "0.0.0.0";
+const port = process.env.PORT || 10000;
+const hostname = process.env.HOSTNAME || "0.0.0.0";
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end("Hello World!");
   })
-  .listen(port);
+  .listen(port, hostname);
 
 console.log(`Server running at http://${hostname}:${port}/`);
